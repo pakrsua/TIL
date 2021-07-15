449b346ce35030de4ebd8385b7f2ef7a27e429e3
unknown <{tndk4997@naver.com}>
2021-07-15 11:52:39 +0900

    git upload practice

---
+++ dust_check.py
@@ -0,0 +1,25 @@
+150
+80
+30
+
+dust = 55
+
+# if dust > 150 :
+#     print("매우나쁨")
+# else:
+#     if dust > 80:
+#         print("나쁨")
+#     else:
+#         if dust > 30:
+#             print("보통")
+#         else:
+#             print("좋음")
+
+if dust > 150:
+    print("매우나쁨")
+elif dust > 80:
+    print("나쁨")
+elif dust > 30:
+    print("보통")
+else:
+    print("좋음")
---
+++ lunch.py
@@ -0,0 +1,8 @@
+import random
+
+menu = ['파스타','김치','피자']
+
+print(menu)
+
+my_lunch = random.choice(menu)
+print(my_lunch)
