716813e9b5a25c2b26b9bb914eb8c9dd5e7d3157
park su ah <tndk4997@naver.com>
2021-08-27 23:56:28 +0900

    0827

--- python/boj/단계별로 풀어보기/while/1110.py
+++ python/boj/단계별로 풀어보기/while/1110.py
@@ -2,10 +2,6 @@
 
 cnt = 0
 
-# if N < 10:
-#     print(N*11)
-#
-# print(((N&10)*10) + ((N%10)+(N//10))%10)
 new_num = int(input())
 N = new_num
 
@@ -15,7 +11,6 @@ while True:
     else:
         second = new_num//10 + new_num % 10
         new_num = (new_num%10)*10 + (second%10)
-        # print(new_num)
         cnt += 1
 
 print(cnt)
