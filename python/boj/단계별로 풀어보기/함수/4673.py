90c2a08f59545bfd52f47170f792062ca995bbd6
park su ah <tndk4997@naver.com>
2021-09-08 23:31:50 +0900

    0908

--- python/boj/단계별로 풀어보기/함수/4673.py
+++ python/boj/단계별로 풀어보기/함수/4673.py
@@ -1,2 +1,14 @@
 # 셀프 넘버
 
+# 생성자 리스트
+def self_num(d):
+    if d < 10000:
+        num = []
+        for i in range(1, d+1):
+            n = i
+            n += int(str(i))
+            n
+            print(n)
+    return True
+for i in range(1,10001):
+    print(self_num(i))
