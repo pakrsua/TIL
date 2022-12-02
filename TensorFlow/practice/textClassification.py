60bb4ecac5a5d5d8773b04dd3ad51ad628df17e0
park su ah <tndk4997@naver.com>
2022-12-02 23:04:12 +0900

    영화 리뷰를 사용한 텍스트 분류

---
+++ TensorFlow/practice/textClassification.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+print(tf.__version__)
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+print("훈련 샘플: {}, 레이블: {}".format(len(train_data), len(train_labels)))
+print('train_data',train_data[0])
