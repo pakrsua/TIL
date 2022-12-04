8180b8597185a5de2b780d7e3c4144cad53110cb
park su ah <tndk4997@naver.com>
2022-12-04 23:31:48 +0900

    영화 리뷰를 사용한 텍스트 분류 -ing

--- TensorFlow/practice/textClassification.py
+++ TensorFlow/practice/textClassification.py
@@ -9,5 +9,24 @@ imdb = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
-print("훈련 샘플: {}, 레이블: {}".format(len(train_data), len(train_labels)))
-print('train_data',train_data[0])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+# print("훈련 샘플: {}, 레이블: {}".format(len(train_data), len(train_labels)))
+# print('train_data',train_data[0])
+len(train_data[0]), len(train_data[1])
+# 단어와 정수 인덱스를 매핑한 딕셔너리
+word_index = imdb.get_word_index()
+
+# 처음 몇 개 인덱스는 사전에 정의되어 있습니다
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+decode_review(train_data[0])
