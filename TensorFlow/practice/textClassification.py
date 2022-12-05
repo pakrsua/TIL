30bdcc223f5c7da0653f9916e9e4093a4637303c
park su ah <tndk4997@naver.com>
2022-12-05 23:03:10 +0900

    영화 리뷰를 사용한 텍스트 분류 -ing;

--- TensorFlow/practice/textClassification.py
+++ TensorFlow/practice/textClassification.py
@@ -30,3 +30,43 @@ word_index["<UNUSED>"] = 3
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 decode_review(train_data[0])
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding='post', maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding='post', maxlen=256)
+
+len(train_data[0]), len(train_data[1])
+# print(train_data[0])
+
+
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16, input_shape=(None,)))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation='relu'))
+model.add(keras.layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+# 모델 훈련
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+# 모델 평가
+results = model.evaluate(test_data,  test_labels, verbose=2)
+
+print(results)
